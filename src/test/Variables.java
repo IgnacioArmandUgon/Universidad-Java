e312051cc967b5870edfdeeff4f2cf09f0ec170c
Christian Tejeda <christ.0@outlook.com>
2021-12-06 00:31:47 -0300

    Correcciones varias

--- src/test/Variables.java
+++ src/test/Variables.java
@@ -1,25 +1,20 @@
-package Java;
-
-import java.net.StandardSocketOptions;
+package test;
 
 public class Variables {
 
          public static void main(String args[]) {
              int a = (int)10E2;
-             var laVar = "asd";
-             String name = "Nacho";
+             String name = "Chris";
              System.out.println();
-             System.out.println("Hola mundo");
+             System.out.println("¡Hola, mundo!");
 
              char decimalUnicode = 92;
              System.out.println(decimalUnicode);
 
-             boolean mayor = a>100;
+             boolean mayor = a > 100;
 
-             var num = 2;
-             var resultado = (num % 2 == 0) ? "es par" : "no es par";
+             int num = 2;
+             String resultado = (num % 2 == 0) ? "es par" : "no es par";
              System.out.println(num + " " + resultado);
-
          }
-
 }
